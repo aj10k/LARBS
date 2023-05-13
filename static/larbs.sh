ea38b8c451ec4b15ee15d6182913d6308017fee7
aj10k <28464125+aj10k@users.noreply.github.com>
2023-05-13 16:44:46 -0400

    test program

--- static/larbs.sh
+++ static/larbs.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
+dotfilesrepo="https://github.com/aj10k/voidrice.git"
+progsfile="https://raw.githubusercontent.com/aj10k/LARBS/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
 export TERM=ansi
